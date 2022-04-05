fe994cecaa95d29cd7139537c2fad3fbdc725748
rosnbro <benro95@gmail.com>
2022-04-05 12:19:02 -0400

    refined style and completed basic functionality

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const TicTacToe = (function() {
     
     // DOM
     const cells = document.querySelectorAll(".cell");
+    const difficulty = document.querySelector("#difficulty");
     const xButton = document.querySelector("#selectX");
     const oButton = document.querySelector("#selectO");
-    const difficulty = document.querySelector("#difficulty");
+    const resetButton = document.querySelector("#reset");
+    const playerScore = document.querySelector("#playerScore");
+    const computerScore = document.querySelector("#computerScore");
 
     // Game objects
     const Gameboard = {
@@ -13,7 +16,6 @@ const TicTacToe = (function() {
             cells.forEach((cell, i) => {
                 cell.addEventListener("click", () => {
                     if (this.board[i] == "") {
-                        //addSymbol(i);
                         Player.move(i);
                     }
                 });
@@ -23,7 +25,6 @@ const TicTacToe = (function() {
             cells.forEach((cell, i) => {
                 cell.innerHTML = this.board[i];
             });
-            checkForWin();
         },
         reset: function() {
             this.board = ["","","","","","","","",""];
@@ -32,138 +33,136 @@ const TicTacToe = (function() {
     }
 
     const Player = {
-        symbol: "X", /////////////////////// set so if O computer goes first
+        symbol: "X",
+        wins: 0,
         listen: function() {
             xButton.addEventListener("click", () => {
                 this.symbol = "X";
+                Computer.symbol = "O"
                 Gameboard.reset();
+                xButton.classList.add("active");
+                oButton.classList.remove("active");
             });
             oButton.addEventListener("click", () => {
                 this.symbol = "O";
+                Computer.symbol = "X"
+                Gameboard.reset();
+                Computer.move();
+                oButton.classList.add("active");
+                xButton.classList.remove("active");
+            });
+            resetButton.addEventListener("click", () => {
                 Gameboard.reset();
             });
         },
         move: function(i) {
             Gameboard.board.splice(i, 1, this.symbol);
             Gameboard.render();
+            if (Gameplay.checkForWin() == false) Computer.move();
         },
     }
     
     const Computer = {
-        symbol: function() {
-            if (Player.symbol == "X") {
-                return "O";
-            } else return "X";
-        },
+        symbol: "O",
+        wins: 0,
         move: function() {
-            switch (difficulty) {
-                case easy:
-                    // Randomly fill one empty place
-                    Gameboard.board.forEach((space, i) => {
-                        if (space == "") {
-
-                        }
-                    });
-                    break;
-                case moderate:
+            switch (difficulty.value) {
+                case "moderate":
                     // Alternate between random and expert
                     break;
-                case draw:
+                case "draw":
                     // Expert but choose only draw scenarios
                     break;
-                case expert:
+                case "expert":
                     // best possible moves
                     break;
                 default:
-                    // copy code for easy
+                    let isPlaced = false;
+                    let i;
+                    do {
+                        i = Math.floor(Math.random() * Gameboard.board.length);
+                        if (Gameboard.board[i] == "") {
+                            Gameboard.board.splice(i, 1, this.symbol);
+                            isPlaced = true;
+                        }
+                    } while (isPlaced == false);
+                    Gameboard.render();
+                    Gameplay.checkForWin();
                     break;
             }
         },
-        
+    }
+
+    const Gameplay = {
+        start: function() {
+            Player.listen();
+            Gameboard.listen();
+        },
+        checkForWin: function() {
+            if (Gameboard.board[0] == Gameboard.board[1] &&
+                Gameboard.board[0] == Gameboard.board[2] &&
+                Gameboard.board[0] != "") {
+                    this.declareWinner(0);
+            } else
+            if (Gameboard.board[3] == Gameboard.board[4] &&
+                Gameboard.board[3] == Gameboard.board[5] &&
+                Gameboard.board[3] != "") {
+                    this.declareWinner(3);
+            } else
+            if (Gameboard.board[6] == Gameboard.board[7] &&
+                Gameboard.board[6] == Gameboard.board[8] &&
+                Gameboard.board[6] != "") {
+                    this.declareWinner(6);
+            } else
+            if (Gameboard.board[0] == Gameboard.board[3] &&
+                Gameboard.board[0] == Gameboard.board[6] &&
+                Gameboard.board[0] != "") {
+                    this.declareWinner(0);
+            } else
+            if (Gameboard.board[1] == Gameboard.board[4] &&
+                Gameboard.board[1] == Gameboard.board[7] &&
+                Gameboard.board[1] != "") {
+                    this.declareWinner(1);
+            } else
+            if (Gameboard.board[2] == Gameboard.board[5] &&
+                Gameboard.board[2] == Gameboard.board[8] &&
+                Gameboard.board[2] != "") {
+                    this.declareWinner(2);
+            } else
+            if (Gameboard.board[0] == Gameboard.board[4] &&
+                Gameboard.board[0] == Gameboard.board[8] &&
+                Gameboard.board[0] != "") {
+                    this.declareWinner(0);
+            } else
+            if (Gameboard.board[2] == Gameboard.board[4] &&
+                Gameboard.board[2] == Gameboard.board[6] &&
+                Gameboard.board[2] != "") {
+                    this.declareWinner(2);
+            } else 
+            if (!Gameboard.board.includes("")) {
+                    this.declareWinner("tie");
+            } else return false;
+        },
+        declareWinner: function(i) {
+            if (i == "tie") {
+                this.displayWinner("Nobody");
+            } else if (Gameboard.board[i] == Player.symbol) {
+                Player.wins++;
+                this.displayWinner("You");
+            } else {
+                Computer.wins++;
+                this.displayWinner("The Computer");
+            }
+        },
+        displayWinner: function(winner) {
+            alert(winner + " won this one!");
+            playerScore.innerHTML = Player.wins;
+            computerScore.innerHTML = Computer.wins;
+            // Gameboard.reset();
+        },
     }
 
     // Initialization
-    start();
+    Gameplay.start();
 
-    // Functions
-    function start() {
-        Player.listen();
-        Gameboard.listen();
-    }
-
-    /*function render() {
-        cells.forEach((cell, i) => {
-            cell.innerHTML = Gameboard.board[i];
-        });
-        checkForWin();
-    }*/
-
-    /*function addSymbol(i) { // Make into player and computer methods?
-        Gameboard.board.splice(i, 1, Player.symbol);
-        Gameboard.render();
-    }*/
-
-    function checkForWin() {
-        if (Gameboard.board[0] == Gameboard.board[1] &&
-            Gameboard.board[0] == Gameboard.board[2] &&
-            Gameboard.board[0] != "") {
-                declareWinner(0);
-            } else
-        if (Gameboard.board[3] == Gameboard.board[4] &&
-            Gameboard.board[3] == Gameboard.board[5] &&
-            Gameboard.board[3] != "") {
-                declareWinner(3);
-            } else
-        if (Gameboard.board[6] == Gameboard.board[7] &&
-            Gameboard.board[6] == Gameboard.board[8] &&
-            Gameboard.board[6] != "") {
-                declareWinner(6);
-            } else
-        if (Gameboard.board[0] == Gameboard.board[3] &&
-            Gameboard.board[0] == Gameboard.board[6] &&
-            Gameboard.board[0] != "") {
-                declareWinner(0);
-            } else
-        if (Gameboard.board[1] == Gameboard.board[4] &&
-            Gameboard.board[1] == Gameboard.board[7] &&
-            Gameboard.board[1] != "") {
-                declareWinner(1);
-            } else
-        if (Gameboard.board[2] == Gameboard.board[5] &&
-            Gameboard.board[2] == Gameboard.board[8] &&
-            Gameboard.board[2] != "") {
-                declareWinner(2);
-            } else
-        if (Gameboard.board[0] == Gameboard.board[4] &&
-            Gameboard.board[0] == Gameboard.board[8] &&
-            Gameboard.board[0] != "") {
-                declareWinner(0);
-            } else
-        if (Gameboard.board[2] == Gameboard.board[4] &&
-            Gameboard.board[2] == Gameboard.board[6] &&
-            Gameboard.board[2] != "") {
-                declareWinner(2);
-            } else
-        if (!Gameboard.board.includes("")) {
-                declareWinner("tie");
-            }
-    }
-
-    function declareWinner(i) {
-        if (i == "tie") {
-            console.log("it's a tie!");
-        }
-        if (Gameboard.board[i] == Player.symbol) {
-            console.log("You won!");
-            // pass winner to results screen
-            // player win count +1
-        } else {
-            console.log("You lost...");
-            // pass winner to results screen
-            // computer win count +1
-        }
-        
-    }
-
-    // Add dom methods to add results screen //////////////////////
 })();
